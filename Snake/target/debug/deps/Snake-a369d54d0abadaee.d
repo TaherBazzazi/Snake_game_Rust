f291a381a18cbd5117a445a369488c6b52df4785
Taher_Bazzazi <Taher.bazzazi10@gmail.com>
2023-07-05 17:36:35 +0200

    project creation

---
+++ Snake/target/debug/deps/Snake-a369d54d0abadaee.d
@@ -0,0 +1,5 @@
+C:\Users\abgad\OneDrive\Documents\Snake_Game_Rust\Snake\target\debug\deps\Snake-a369d54d0abadaee.rmeta: src\main.rs
+
+C:\Users\abgad\OneDrive\Documents\Snake_Game_Rust\Snake\target\debug\deps\Snake-a369d54d0abadaee.d: src\main.rs
+
+src\main.rs:
---
+++ Snake/target/debug/deps/Snake-c85042fae1cca760.d
@@ -0,0 +1,5 @@
+C:\Users\abgad\OneDrive\Documents\Snake_Game_Rust\Snake\target\debug\deps\Snake-c85042fae1cca760.rmeta: src\main.rs
+
+C:\Users\abgad\OneDrive\Documents\Snake_Game_Rust\Snake\target\debug\deps\Snake-c85042fae1cca760.d: src\main.rs
+
+src\main.rs:
